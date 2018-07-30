f80009bf7544dbee1804c82b3fea6014ccb18771
Shuai Zhang <zhangshuai.ustc@gmail.com>
2018-07-30 22:05:54 +0800

    Print path, creationTime, lastModifiedTime, size, checksum for file.

--- WORKSPACE
+++ WORKSPACE
@@ -6,6 +6,21 @@ maven_server(
 )
 
 maven_jar(
+    name = "commons_codec_commons_codec",
+    artifact = "commons-codec:commons-codec:1.11"
+)
+
+maven_jar(
+    name = "org_bouncycastle_bcprov_jdk15on",
+    artifact = "org.bouncycastle:bcprov-jdk15on:1.60"
+)
+
+maven_jar(
+    name = "com_google_guava_guava",
+    artifact = "com.google.guava:guava:25.1-jre"
+)
+
+maven_jar(
     name = "org_slf4j_slf4j_api",
     artifact = "org.slf4j:slf4j-api:1.7.25"
 )
@@ -18,4 +33,13 @@ maven_jar(
 maven_jar(
     name = "ch_qos_logback_logback_core",
     artifact = "ch.qos.logback:logback-core:1.2.3"
+)
+maven_jar(
+    name = "junit_junit",
+    artifact = "junit:junit:4.12"
+)
+
+maven_jar(
+    name = "com_google_jimfs_jimfs",
+    artifact = "com.google.jimfs:jimfs:1.1"
 )
--- src/main/java/io/github/hcoona/directory_manifest/Controller.java
+++ src/main/java/io/github/hcoona/directory_manifest/Controller.java
@@ -1,5 +1,7 @@
 package io.github.hcoona.directory_manifest;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,23 +13,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.security.Security;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.EnumSet;
 
 class Controller {
   private static final Logger LOG = LoggerFactory.getLogger(Controller.class);
 
-  private final FileSystem fs;
+  private static final DateTimeFormatter ISO_8601_FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss'Z'");
+  private static final DigestUtils digest;
+
+  static {
+    Security.addProvider(new BouncyCastleProvider());
+    digest = new DigestUtils("WHIRLPOOL");
+  }
+
   private final Path dirPath;
 
   Controller(FileSystem fs, String dir) {
-    this.fs = fs;
     this.dirPath = fs.getPath(dir);
   }
 
   public void run() throws IOException {
+    ManifestFileVisitor manifestFileVisitor = new ManifestFileVisitor();
     Files.walkFileTree(
         dirPath, EnumSet.of(FileVisitOption.FOLLOW_LINKS),
-        Integer.MAX_VALUE, new ManifestFileVisitor());
+        Integer.MAX_VALUE, manifestFileVisitor);
   }
 
   class ManifestFileVisitor extends SimpleFileVisitor<Path> {
@@ -50,8 +67,20 @@ class Controller {
     @Override
     public FileVisitResult visitFile(
         Path file, BasicFileAttributes attrs) throws IOException {
-      LOG.info(file.toString());
+      String checksum = digest.digestAsHex(file.toFile());
+      LOG.info(String.join(",", Arrays.asList(
+          file.toString(),
+          formatFileTime(attrs.creationTime()),
+          formatFileTime(attrs.lastModifiedTime()),
+          String.valueOf(attrs.size()),
+          checksum)));
       return super.visitFile(file, attrs);
     }
+  }
+
+  private static String formatFileTime(FileTime fileTime) {
+    long cTime = fileTime.toMillis();
+    ZonedDateTime t = Instant.ofEpochMilli(cTime).atZone(ZoneId.of("UTC"));
+    return ISO_8601_FORMATTER.format(t);
   }
 }
