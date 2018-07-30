68dbe6783c72dec7198cd341af57fe627f8ba581
Shuai Zhang <zhangshuai.ustc@gmail.com>
2018-07-30 21:16:22 +0800

    Initial commit.

---
+++ WORKSPACE
@@ -0,0 +1,6 @@
+load("@bazel_tools//tools/build_defs/repo:maven_rules.bzl",
+    "maven_jar", "maven_dependency_plugin")
+
+maven_server(
+    name = "default"
+)
---
+++ src/main/java/io/github/hcoona/directory_manifest/App.java
@@ -0,0 +1,7 @@
+package io.github.hcoona.directory_manifest;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
